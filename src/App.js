beed9cf2b9ff9ffa619dadea74f24260458e85f6
Kevin Garza <kevin_4569@msn.com>
2022-01-15 10:16:05 -0600

    Finalizando el ejercicio de lista de tareas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import FormularioTareas from './components/FormularioTareas';
+import Header from './components/Header';
+import ListaTareas from './components/ListaTareas';
 
-function App() {
+const App = () => {
+  const tareasGuardadas = 
+          localStorage.getItem('tareas') 
+          ? 
+         JSON.parse(localStorage.getItem('tareas')) 
+          : 
+          [];
+
+
+let configMostrarCompletadas = '';
+if(localStorage.getItem('completadas' === null)){
+  configMostrarCompletadas = true;
+}  else {
+   configMostrarCompletadas = localStorage.getItem('completadas') === 'true';
+}
+
+
+  const [tareas, setTareas] = useState(tareasGuardadas);
+
+useEffect(() => {
+   localStorage.setItem('tareas', JSON.stringify(tareas));
+   
+}, [tareas]);
+
+const [mostrarCompletadas, setMostrarCompletadas] = useState(configMostrarCompletadas);
+
+useEffect(() => {
+  localStorage.setItem('completadas', mostrarCompletadas.toString());
+  
+}, [mostrarCompletadas]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="contenedor">
+      <Header  
+          mostrarCompletadas = {mostrarCompletadas}
+          setMostrarCompletadas = {setMostrarCompletadas}   
+      />
+      <FormularioTareas tareas={tareas} setTareas={setTareas}/>
+      <ListaTareas 
+          tareas={tareas}   
+          setTareas={setTareas} 
+          mostrarCompletadas = {mostrarCompletadas}
+      />
     </div>
   );
 }
